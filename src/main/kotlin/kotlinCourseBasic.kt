8cbb07a198b74f749c11c45a8638f223a632e2c9
Masa <mohammed19971985@gmail.com>
2023-02-03 23:20:21 +0300

    range and when statment

--- src/main/kotlin/kotlinCourseBasic.kt
+++ src/main/kotlin/kotlinCourseBasic.kt
@@ -19,6 +19,7 @@ fun main() {
    // println(link.toDouble()) // not run
     println(link.isEmpty())
     statmentIf(isBlocked)
+    println(getAgeStageRange(like))
 }
 
 fun statmentIf(isBlock:Boolean){
@@ -27,4 +28,18 @@ fun statmentIf(isBlock:Boolean){
     }else {
         println("user is not blocked")
     }
+}
+fun getAgeStageRange(age: Int):String{
+     var result = when (age){
+        in -1 downTo Int.MIN_VALUE -> "بعدك مجاي للدنيا"
+        in 0..4 -> "رضيع"
+        in 5..12 -> "طفل"
+        in 13..19 -> "مراهق "
+        in 20..39 -> " شاب"
+        in 40..59 -> " رجل"
+        in 60..120 -> " شايب"
+        in 121..Int.MAX_VALUE ->"الله يرحمة جان خوش انسان"
+        else -> "دخل عمرك عدك *_^"
+    }
+    return result
 }
