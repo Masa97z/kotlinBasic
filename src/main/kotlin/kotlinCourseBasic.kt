c49d417af46385af2cff659bfb412c1f71e4f899
Masa <mohammed19971985@gmail.com>
2023-02-03 23:14:40 +0300

    if statment method 05 lesson

--- src/main/kotlin/kotlinCourseBasic.kt
+++ src/main/kotlin/kotlinCourseBasic.kt
@@ -7,6 +7,7 @@ val link :String = "www.masaso.com"
 var like : Int = 6
 var price :Float = 3.4f
 var city: String? = null
+var isBlocked  = true
 
 fun main() {
     // local variable jest in function
@@ -17,4 +18,13 @@ fun main() {
     println(link.toUpperCase())
    // println(link.toDouble()) // not run
     println(link.isEmpty())
+    statmentIf(isBlocked)
+}
+
+fun statmentIf(isBlock:Boolean){
+    if (isBlock){
+        println("uesr is blocked")
+    }else {
+        println("user is not blocked")
+    }
 }
