edf0daee3f96bcb34158b097b1b99c098032c164
Masa <mohammed19971985@gmail.com>
2023-02-03 22:43:49 +0300

    type casting

---
+++ src/main/kotlin/kotlinCourseBasic.kt
@@ -0,0 +1,15 @@
+/**
+ * 01 kotlin variable
+ *
+ */
+// global variable
+val link :String = "www.masaso.com"
+var like : Int = 6
+var price :Float = 3.4f
+
+fun main() {
+    // local variable jest in function
+    var prices: Double = 7.14
+    println(prices.toString())
+    print(link)
+}
